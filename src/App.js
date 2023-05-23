0222e284922fcc9802b94a57c7434a31e1afbeea
KYEONGMIN CHO <ggingmin@gmail.com>
2023-05-23 15:50:50 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          클라우드타입
+          cloudtype
         </p>
         <a
           className="App-link"
